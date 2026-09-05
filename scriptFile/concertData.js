e3f0a86ae27acec64fa3ffa20e2a3e301aba7d2d
bindushishir1 <bindushishir004@gmail.com>
2026-09-05 13:38:10 +0545

    chore:folder structure is created

---
+++ scriptFile/concertData.js
@@ -0,0 +1,34 @@
+const event = [
+  {
+    id: 1,
+    title: "Kathmandu Music Festival",
+    artist: "The Elements",
+    category: "Music",
+    image: "/images/concerts/kathmandu-music.jpg",
+    description:
+      "A live music festival featuring popular Nepali artists and bands.",
+    date: "2026-10-10",
+    time: "6:00 PM",
+    venue: "Tundikhel Ground",
+    city: "Kathmandu",
+    duration: "4 hours",
+    ticket:[]
+  },
+
+  {
+    id: 2,
+    title: "shishir private show",
+    artist: "sujan chapagain",
+    category: "Music",
+    image: "",
+    description:
+      "A live music on the occasion of wife's birthday",
+    date: "2026-10-10",
+    time: "6:00 PM",
+    venue: "Shishir Niwas",
+    city: "Bajura",
+    duration: "3 hours",
+    ticket:[]
+  },
+
+];
